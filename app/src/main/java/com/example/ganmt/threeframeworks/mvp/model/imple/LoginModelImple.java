d1dafa304e037623940a022c1e7e046218d575cb
ganmingtao@126.com <just4youlm>
2016-05-31 14:48:00 +0800

    首先

--- app/src/main/java/com/example/ganmt/threeframeworks/mvp/model/imple/LoginModelImple.java
+++ app/src/main/java/com/example/ganmt/threeframeworks/mvp/model/imple/LoginModelImple.java
@@ -29,39 +29,6 @@ public class LoginModelImple implements LoginModel {
     }
     @Override
     public void login(String username, String password, final OnLoginListener onLoginListener) {
-        /*String url = "";
-        RequestParams requestParams = new RequestParams();
-        requestParams.put("name", name);
-        requestParams.put("password", password);
-        //网络请求
-        HttpUtil.post(url, requestParams, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                //登录成功
-                String json = new String(responseBody);
-                try {
-                    JSONObject jsonObject = new JSONObject(json);
-                    int code = 0;
-                    code = jsonObject.getJSONObject("data").getInt("code");
-                    //code=0 表示登录成功，code=1表示用户名错误，code=2表示密码错误
-                    if (code == 0) {
-                        onLoginListener.onSuccess();
-                    } else if (code == 1) {
-                        onLoginListener.onUsernameError();
-                    } else if (code == 2) {
-                        onLoginListener.onPasswordError();
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                onLoginListener.onFailure();
-            }
-        });*/
-        //binding.username.getText().toString()
         ReqLogin reqLogin = new ReqLogin();
         reqLogin.setUsername(username);
         reqLogin.setPassword(password);
